1f1120ec613c50ed12197ca7b2cefcdbc31b9b51
Wojtazz <marcin.witas72@gmail.com>
2024-11-03 17:27:55 +0100

    feat: setup auth0

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,14 @@
 import { type ReactNode } from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 type DashboardLayoutProps = Readonly<{
 	children: ReactNode;
 }>;
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-	return <main>{children}</main>;
+	return (
+		<UserProvider>
+			<main>{children}</main>
+		</UserProvider>
+	);
 }
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,10 @@
-export default function DashboardPage() {
-	return <div>Hello World from dashboard!</div>;
+import { LoginButton } from '@/components/login-button';
+
+export default async function DashboardPage() {
+	return (
+		<div>
+			<div>Hello World from dashboard!</div>
+			<LoginButton />
+		</div>
+	);
 }
